8758172662346baa0d7a002fe347892f172b53ec
nekekii <icyduckblock@gmail.com>
2017-09-10 13:52:48 -0600

    Disable slideout animation for mobile

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
     $("#text").css("right", "100vw");
     $("#text").animate({right: '0vw'}, 1000);
     $("img").click(function(){
-        console.log("clicked");
-        $("#text").animate({left: '100vw'}, 1500, function(){
-          console.log("Have a nice day!");
+        //disable for mobile
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
           window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
-        });
+        }else{
+          $("#text").animate({left: '100vw'}, 1500, function(){
+            console.log("Have a nice day!");
+            window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+          });
+        }
     });
 });
