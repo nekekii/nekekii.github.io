767408ffcbc22eab5a9aabb3e22ae4c791f5a72a
nekekii <icyduckblock@gmail.com>
2017-09-10 15:12:54 -0600

    Add more social media

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -1,14 +1,39 @@
 $(document).ready(function(){
     console.log("Welcome to my JS, brave traveler.")
-    $("#text").css("right", "100vw");
-    $("#text").animate({right: '0vw'}, 1000);
-    $("img").click(function(){
+    $("#text").css("top", "-100vh");
+    $("#text").animate({top: '25vh'}, 1000);
+
+    $("#steam").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
           window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
         }else{
-          $("#text").animate({left: '100vw'}, 1500, function(){
-            console.log("Have a nice day!");
+          $("#text").animate({left: '-100vw'}, 1000, function(){
+            console.log("Have a nice day! See you on steam.");
+            window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+          });
+        }
+    });
+
+    $("#sc").click(function(){
+        //disable for mobile
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          window.location.href = "http://snapchat.com/add/nekekii";
+        }else{
+          $("#text").animate({top: '100vw'}, 1000, function(){
+            console.log("Have a nice day! See you on Snapchat.");
+            window.location.href = "http://snapchat.com/add/nekekii";
+          });
+        }
+    });
+
+    $("#gh").click(function(){
+        //disable for mobile
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+        }else{
+          $("#text").animate({left: '100vw'}, 1000, function(){
+            console.log("Have a nice day! See you on GitHub.");
             window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
           });
         }
