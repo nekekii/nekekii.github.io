56b2afe1d8b2d854888454bdfc61f6f52a7817f8
nekekii <icyduckblock@gmail.com>
2017-09-10 13:41:27 -0600

    Add animations
    + Animation for text every 10 seconds
    + Animation on start up for slide in
    + JA
    + Animation to slide out and then go to page
    + Pointer icon on hover of image, because it is not an a tag.
    + New js file called pagetransition, replaces tonekeki
    + JQuery
    + UTF-8 meta so no more DecodeURI

---
+++ js/pagetransition.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log("Welcome to my JS, brave traveler.")
+    $("#text").css("right", "60vw");
+    $("#text").animate({right: '0vw'}, 1000);
+    $("img").click(function(){
+        console.log("clicked");
+        $("#text").animate({left: '60vw'}, 1500, function(){
+          console.log("Have a nice day!");
+          window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+        });
+    });
+});
--- js/tonekeki.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("2").innerHTML = decodeURI("%E3%81%AD%E3%81%91%E3%81%8D");
