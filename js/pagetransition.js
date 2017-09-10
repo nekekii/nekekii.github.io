8e2ab9c5fe60734918d39dc3d843ee484bb3e3a7
nekekii <icyduckblock@gmail.com>
2017-09-10 15:37:43 -0600

    Fix github link and add noscript support

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     console.log("Welcome to my JS, brave traveler.")
+    $("#social").html('<img src="images/steam.png" id="steam"><img src="images/snapchat.png" id="sc"><img src="images/github.png" id="gh">"');
     $("#text").css("top", "-100vh");
     $("#text").animate({top: '25vh'}, 1000);
 
@@ -30,11 +31,11 @@ $(document).ready(function(){
     $("#gh").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+          window.location.href = "https://github.com/nekekii/";
         }else{
           $("#text").animate({left: '100vw'}, 1000, function(){
             console.log("Have a nice day! See you on GitHub.");
-            window.location.href = "http://steamcommunity.com/profiles/76561198071986407";
+            window.location.href = "https://github.com/nekekii/";
           });
         }
     });
