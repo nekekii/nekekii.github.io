cb3f07ca3ced22c4f655b1315831b9033db43977
nekekii <icyduckblock@gmail.com>
2017-12-28 14:39:00 -0700

    Update color theme and links
    This commit changes the color theme from a dark, black and purple color
    theme to a light, white and blue color theme. The favicon has been
    updated to reflect this.
    
    This also removes the Snapchat link and adds the image, link, and
    styling for Twitter.
    
    Some old Snapchat files, such as the styling and logo image will stay in
    case it is to be used later.

--- js/pagetransition.js
+++ js/pagetransition.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     console.log("Welcome to my JS, brave traveler.");
     console.log("Don't ask.");
     console.log("this is cool I guess...");
-    $("#social").html('<img src="images/steam.png" id="steam"><img src="images/snapchat.png" id="sc"><img src="images/github.png" id="gh">"');
+    $("#social").html('<img src="images/steam.png" id="steam"><img src="images/twitter.png" id="tw"><img src="images/github.png" id="gh">');
     $("#text").css("top", "-100vh");
     $("#text").animate({top: '25vh'}, 1000);
 
@@ -18,14 +18,14 @@ $(document).ready(function(){
         }
     });
 
-    $("#sc").click(function(){
+    $("#tw").click(function(){
         //disable for mobile
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-          window.location.href = "http://snapchat.com/add/nekekii";
+          window.location.href = "https://twitter.com/nekekiii";
         }else{
           $("#text").animate({top: '100vw'}, 1000, function(){
-            console.log("Have a nice day! See you on Snapchat.");
-            window.location.href = "http://snapchat.com/add/nekekii";
+            console.log("Have a nice day! See you on Twitter.");
+            window.location.href = "https://twitter.com/nekekiii";
           });
         }
     });
