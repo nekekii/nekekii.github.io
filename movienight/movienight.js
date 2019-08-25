8270e7913b2dc7d0220ee0b1a5fab11bbefbe9df
nekekii <nekeki@protonmail.com>
2019-08-25 08:16:35 -0600

    Make IMPORTANT text BIG

--- movienight/movienight.js
+++ movienight/movienight.js
@@ -51,7 +51,7 @@ function earlyText() {
 
 function runningText() {
   if (stage !== 2) {
-    $( "#starttext" ).html( "<p>To start watching and sync the movie with everyone else, <span class=\"rainbow\">double click</span> in the center of the page.</p>" );
+    $( "#starttext" ).html( "<p>To start watching and sync the movie with everyone else, <span class=\"rainbow\"><h2>double click</h2></span> in the center of the page.</p>" );
     stage = 2;
   }
   if(watching == false) {
