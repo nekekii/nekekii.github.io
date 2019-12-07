d5aca3ec1cf7750d725fa413cf673175fc1e0d21
nekeki <nekeki@protonmail.com>
2019-12-07 09:08:25 -0700

    Testing for YouTube compatibility

--- movienight/movienight.js
+++ movienight/movienight.js
@@ -1,7 +1,8 @@
-var moviename = "Ocean's Eight (2018)";
-var movieid = "1BFQ2xg8IOfBfneOJlKZb0MYEi0GU46pM";
-var moviestart = 1574532000000; //2019 11 23 6pm UK 1574532000000
-var movieend = 1574538660000; //1 hour 51 minutes 1574538660000
+var moviename = "Bob Ross - One Hour Special - The Grandeur of Summer"; //Name of Video
+var movieid = "mT0RNrTDHkI"; //Video ID
+var moviestart = 1575735300000; //UTC Start Time
+var movieend = 1575738900000; //UTC End Time
+var service = "youtube"  //Currently compatible: drive, youtube
 var tid;
 var currentdate;
 var stage = 0;
@@ -50,32 +51,48 @@ function earlyText() {
 }
 
 function runningText() {
-  if (stage !== 2) {
-    refreshVideo();
-    $( "#starttext" ).html( "<p>To start watching and sync the movie with everyone else, <h2><span class=\"rainbow\">double click</span></h2> in the center of the page.</p>" );
-    stage = 2;
-  }
-  if(watching == false) {
-    if(counter > 2) {
-      counter = 0;
-      if(stale <= maxstale) {
-        refreshVideo();
-        stale ++;
+  if(service == "drive") {
+    if (stage !== 2) {
+      refreshVideo();
+      $( "#starttext" ).html( "<p>To start watching and sync the movie with everyone else, <h2><span class=\"rainbow\">double click</span></h2> in the center of the page.</p>" );
+      stage = 2;
+    } else if(watching == false) {
+      if(counter > 2) {
+        counter = 0;
+        if(stale <= maxstale) {
+          refreshVideo();
+          stale ++;
+        } else {
+          //console.log("video refresh refused - client stale")
+        }
       } else {
-        //console.log("video refresh refused - client stale")
+        counter ++;
+        //console.log("Counter: " + counter);
       }
-    } else {
-      counter ++;
-      //console.log("Counter: " + counter);
     }
+  } else if(service == "youtube") {
+    if(watching == false) {
+      $( "#starttext" ).html( "<p><h2><span class=\"rainbow\">Loading...</span></h2> Been loading a while? Try and make sure Javascript is enabled and refresh.</p>" );
+      refreshVideo();
+      stage = 2;
+      watching = true;
+      $( "#toclick" ).html( "" ).css( "z-index", "-1");
+    }
+  } else {
+    alert("Error configuring service!");
   }
+
 }
 
 function refreshVideo() {
   currentdate = new Date();
   var timeinto = Math.floor((currentdate.getTime() - moviestart) / 1000);
   //console.log("Updating -- Time Into Movie " + timeinto.toString());
-  $( "#videoframe" ).html( "<div id=\"videoframe\"><iframe src=\"https://drive.google.com/file/d/" + movieid + "/preview?t=" + timeinto.toString() + "\"></iframe>" )
+  if(service == "drive") {
+    $( "#videoframe" ).html( "<div id=\"videoframe\"><iframe src=\"https://drive.google.com/file/d/" + movieid + "/preview?t=" + timeinto.toString() + "\"></iframe>" )
+  } else if(service == "youtube") {
+    $( "#videoframe" ).html( "<div id=\"videoframe\"><iframe src=\"https://youtube.com/embed/" + movieid + "?autoplay=true&start=" + timeinto.toString() + "\"></iframe>" )
+  }
 }
 
 
@@ -87,14 +104,15 @@ $(document).ready(function(){
 
 
   $( "#toclick" ).click(function() {
-
-    if (stage == 2) {
-      if(stale >= maxstale) {
-        console.log("refreshing video cause stale");
-        refreshVideo();
+    if(service == "drive") {
+      if (stage == 2) {
+        if(stale >= maxstale) {
+          console.log("refreshing video cause stale");
+          refreshVideo();
+        }
+        watching = true;
+        $( "#toclick" ).html( "" ).css( "z-index", "-1");
       }
-      watching = true;
-      $( "#toclick" ).html( "" ).css( "z-index", "-1");
     }
   });
 });
